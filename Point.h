22404fb139184ce4f54d09860b267adb9a7f0f3e
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-07-09 21:58:55 +0200

    cleaning files

--- Point.h
+++
@@ -1,9 +0,0 @@
-class Point{
-  private:
-    double x,y;
-  public:
-    Point(const double xx, const double yy): x(xx), y(yy) {};
-
-    double getX() const {return x;};
-    double getY() const {return y;};
-}
