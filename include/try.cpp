adc849c3938e40d95819818a0db20126fa2cc097
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-07-10 21:52:06 +0200

    finished to retrieve the 2nd element of the list

--- include/try.cpp
+++ include/try.cpp
@@ -5,56 +5,118 @@
 using namespace mygal;
 using namespace Rcpp;
 
+// returns if the point "point" is at the boundary of the box "box"
+template <typename T>
+bool isboundary(const Vector2<T>& point, const Box<T>& box){
+  if( ( point.x == box.left | point.x == box.right  ) 
+        & point.y <= box.top & point.y >= box.bottom )
+    return true;
+  if( ( point.y == box.bottom | point.y == box.top  ) 
+        & point.x <= box.right & point.x >= box.left )
+    return true;
+  return false;
+}
+
 // [[Rcpp::export]]
 Rcpp::List computeVoronoiRcpp(const Rcpp::NumericVector& x, const Rcpp::NumericVector& y) {
-  // if( x.size() != y.size() ) std::err ???
-    
-  std::vector<Vector2<double>> points;
+  if( x.size() != y.size() ){
+    Rcpp::Rcerr << "Error! x and y vectors don't have same length!" << std::endl;
+    // i'd like also to exit... maybe not by returning an invalid object
+    return Rcpp::List::create();
+  }
+  
+  typedef long double ftype;  
+  
+  std::vector<Vector2<ftype>> points;
   
   for(int i=0; i<x.size(); i++)
-    points.push_back(Vector2<double>(x[i], y[i]));
+    points.push_back(Vector2<ftype>(x[i], y[i]));
   
-  //auto points = std::vector<Vector2<double>>{{0.354, 0.678}, {0.632, 0.189}, {0.842, 0.942}}; // Generate some points
-  auto algorithm = FortuneAlgorithm<double>(points); // Initialize an instance of Fortune's algorithm
+  // procedure suggested by the author of the library MyGAL
+  auto algorithm = FortuneAlgorithm<ftype>(points); // Initialize an instance of Fortune's algorithm
   algorithm.construct(); // Construct the diagram
   
-  algorithm.bound(Box<double>{-0.05, -0.05, 1.05, 1.05}); // Bound the diagram
+  Box<ftype> localbox{0, 0, 1, 1};
+  algorithm.bound(&localbox); // Bound the diagram
   auto diagram = algorithm.getDiagram(); // Get the constructed diagram
-  diagram.intersect(Box<double>{0.0, 0.0, 1.0, 1.0}); // Compute the intersection between the diagram and a box
+  //diagram.intersect(Box<ftype>{0, 0, 2, 2}); // Compute the intersection between the diagram and a box
   
   auto triangulation = diagram.computeTriangulation();
   
+  // extracting information and generating the R object
+  // matrix of coordinates
   Rcpp::NumericMatrix coord(x.size(), 2);
-  for(int i=0; i<x.size(); i++){
-    coord(i,0) = x[i];
-    coord(i,1) = y[i];
-  }
+  coord(_, 0) = x;
+  coord(_, 1) = y;
+  
+  // data.frame mesh (contains delanuay/voronoi information)
+  Rcpp::IntegerVector ind1, ind2, bp1, bp2;
+  Rcpp::NumericVector x1, y1, x2, y2, mx1, my1, mx2, my2;
+  
+  auto halfedges = diagram.getHalfEdges();
+  for(auto it = halfedges.begin(); it != halfedges.end(); it++)
+    if(it->twin != nullptr){
+      ind1.push_back( it->incidentFace->site->index + 1 );
+      ind2.push_back( it->twin->incidentFace->site->index + 1 );
+      
+      x1.push_back( it->incidentFace->site->point.x );
+      y1.push_back( it->incidentFace->site->point.y );
+      x2.push_back( it->twin->incidentFace->site->point.x );
+      y2.push_back( it->twin->incidentFace->site->point.y );
+      
+      mx1.push_back( it->destination->point.x );
+      my1.push_back( it->destination->point.y );
+      mx2.push_back( it->origin->point.x );
+      my2.push_back( it->origin->point.y );
+      
+      // checking if the half edge is an infinite one
+      bp1.push_back(isboundary(Vector2<ftype>(it->destination->point.x, it->destination->point.y), localbox));
+      bp2.push_back(isboundary(Vector2<ftype>(it->origin->point.x, it->origin->point.y), localbox));
+      
+      // luckily, twin halfedges are stored one after the other so I just need 
+      // to skip the subsequent halfedge to not include the same information twice
+      it++;
+    }
   
   Rcpp::DataFrame mesh = 
-    Rcpp::DataFrame::create(Rcpp::Named("ind1") = 1,
-                            Rcpp::Named("ind2") = 1,
-                            Rcpp::Named("x1") = 1,
-                            Rcpp::Named("y1") = 1,
-                            Rcpp::Named("x2") = 1,
-                            Rcpp::Named("y2") = 1,
-                            Rcpp::Named("mx1") = 1,
-                            Rcpp::Named("my1") = 1,
-                            Rcpp::Named("mx2") = 1,
-                            Rcpp::Named("my2") = 1,
-                            Rcpp::Named("bp1") = 1,
-                            Rcpp::Named("bp2") = 1);
+    Rcpp::DataFrame::create(Rcpp::Named("ind1") = ind1,
+                            Rcpp::Named("ind2") = ind2,
+                            Rcpp::Named("x1") = x1,
+                            Rcpp::Named("y1") = y1,
+                            Rcpp::Named("x2") = x2,
+                            Rcpp::Named("y2") = y2,
+                            Rcpp::Named("mx1") = mx1,
+                            Rcpp::Named("my1") = my1,
+                            Rcpp::Named("mx2") = mx2,
+                            Rcpp::Named("my2") = my2,
+                            Rcpp::Named("bp1") = bp1,
+                            Rcpp::Named("bp2") = bp2);
+  
+  // stuff for tri object (maybe this has better to be done in R)
+  // add here the stuff
   
   Rcpp::List res = Rcpp::List::create(Rcpp::Named("mesh") = mesh,
                                       Rcpp::Named("x") = coord,
-                                      Rcpp::Named(""));
+                                      Rcpp::Named("temp") 
+                                        = Rcpp::NumericVector::create(localbox.left, localbox.right, localbox.bottom, localbox.top));
     
   return res;
 }
 
 /*** R
-n = 5
-set.seed(100)
+# sampling some random points
+n = 50
+errtest = 0; # dummy for testing exiting error (maybe this would be handled by R)
+set.seed(52)
 x = runif(n)
-y = runif(n)
-computeVoronoiRcpp(x, y)
+y = runif(n+errtest)
+
+# loading required packages
+require(alphahull)
+require(rbenchmark)
+
+print("executing test")
+vorcpp = computeVoronoiRcpp(x, y)
+vorR = delvor(x, y)
+# benchmark(computeVoronoiRcpp(x, y), delvor(x, y))
 */
