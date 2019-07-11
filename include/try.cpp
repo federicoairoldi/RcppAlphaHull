f4ac96be56cbfcd096412516a69e016d1e0b26e7
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-07-11 16:10:42 +0200

    finished generation of the mesh object, started the tri.obj

--- include/try.cpp
+++ include/try.cpp
@@ -5,10 +5,11 @@
 using namespace mygal;
 using namespace Rcpp;
 
+/*
 // returns if the point "point" is at the boundary of the box "box"
 template <typename T>
 bool isboundary(const Vector2<T>& point, const Box<T>& box){
-  if( ( point.x == box.left | point.x == box.right  ) 
+  if( ( (point.x == box.left) | (point.x == box.right)  ) 
         & point.y <= box.top & point.y >= box.bottom )
     return true;
   if( ( point.y == box.bottom | point.y == box.top  ) 
@@ -17,13 +18,36 @@ bool isboundary(const Vector2<T>& point, const Box<T>& box){
   return false;
 }
 
+template <typename T>
+bool isboundary2(const Vector2<T>& point, const Box<T>& box){
+  T eps(10e-5);
+  if( ( ( point.x >= (box.left - eps) & point.x <= (box.left + eps) )
+          | ( point.x >= (box.right - eps) & point.x <= (box.right + eps) )  ) 
+        & point.y <= box.top & point.y >= box.bottom )
+        return true;
+  if( ( ( point.y >= (box.top - eps) & point.y <= (box.top + eps) )
+          | ( point.y >= (box.bottom - eps) & point.y <= (box.bottom + eps) )  ) 
+        & point.x <= box.right & point.x >= box.left )
+        return true;
+  return false;
+}*/
+
+template <typename T>
+bool isboundary(const Vector2<T>& point, const Box<T>& box){
+  T eps(10e-5);
+  if( ( std::abs((point.x-box.left)/box.left) < eps | std::abs((point.x-box.right)/box.right) < eps  ) 
+        & point.y <= box.top & point.y >= box.bottom )
+        return true;
+  if( ( std::abs((point.y-box.top)/box.top) < eps | std::abs((point.y-box.bottom)/box.bottom) < eps  ) 
+        & point.x <= box.right & point.x >= box.left )
+        return true;
+  return false;
+}
+
 // [[Rcpp::export]]
 Rcpp::List computeVoronoiRcpp(const Rcpp::NumericVector& x, const Rcpp::NumericVector& y) {
-  if( x.size() != y.size() ){
-    Rcpp::Rcerr << "Error! x and y vectors don't have same length!" << std::endl;
-    // i'd like also to exit... maybe not by returning an invalid object
-    return Rcpp::List::create();
-  }
+  if( x.size() != y.size() )
+    Rcpp::stop("Error! x and y vectors don't have same length!");
   
   typedef long double ftype;  
   
@@ -49,7 +73,7 @@ Rcpp::List computeVoronoiRcpp(const Rcpp::NumericVector& x, const Rcpp::NumericV
   coord(_, 0) = x;
   coord(_, 1) = y;
   
-  // data.frame mesh (contains delanuay/voronoi information)
+  // matrix mesh (contains delanuay/voronoi information)
   Rcpp::IntegerVector ind1, ind2, bp1, bp2;
   Rcpp::NumericVector x1, y1, x2, y2, mx1, my1, mx2, my2;
   
@@ -78,45 +102,64 @@ Rcpp::List computeVoronoiRcpp(const Rcpp::NumericVector& x, const Rcpp::NumericV
       it++;
     }
   
-  Rcpp::DataFrame mesh = 
-    Rcpp::DataFrame::create(Rcpp::Named("ind1") = ind1,
-                            Rcpp::Named("ind2") = ind2,
-                            Rcpp::Named("x1") = x1,
-                            Rcpp::Named("y1") = y1,
-                            Rcpp::Named("x2") = x2,
-                            Rcpp::Named("y2") = y2,
-                            Rcpp::Named("mx1") = mx1,
-                            Rcpp::Named("my1") = my1,
-                            Rcpp::Named("mx2") = mx2,
-                            Rcpp::Named("my2") = my2,
-                            Rcpp::Named("bp1") = bp1,
-                            Rcpp::Named("bp2") = bp2);
+  Rcpp::CharacterVector names{"ind1", "ind2", "x1", "y1", "x2", "y2", "mx1", "my1", "mx2", "my2", "bp1", "bp2"};
+  Rcpp::NumericMatrix mesh(ind1.size(), names.size());
+  colnames(mesh) = names;
+  rownames(mesh) = Rcpp::CharacterVector(ind1.size(), "");
+  
+  mesh(_, 0) = ind1;
+  mesh(_, 1) = ind2;
+  mesh(_, 2) = x1;
+  mesh(_, 3) = y1;
+  mesh(_, 4) = x2;
+  mesh(_, 5) = y2;
+  mesh(_, 6) = mx1;
+  mesh(_, 7) = my1;
+  mesh(_, 8) = mx2;
+  mesh(_, 9) = my2;
+  mesh(_,10) = bp1;
+  mesh(_,11) = bp2;
   
   // stuff for tri object (maybe this has better to be done in R)
-  // add here the stuff
+  // as retrieved from function "tri.mesh" of package tripack here's the structure
+  // of the object tri
+  
+  Rcpp::List tri = Rcpp::List::create(Rcpp::Named("n") = x.size(),
+                                      Rcpp::Named("x") = x,
+                                      Rcpp::Named("y") = y,
+                                      Rcpp::Named("tlist") = 1,
+                                      Rcpp::Named("tlptr") = 1,
+                                      Rcpp::Named("tlend") = 1,
+                                      Rcpp::Named("tlnew") = 1,
+                                      Rcpp::Named("nc") = 0,
+                                      Rcpp::Named("lc") = 0,
+                                      Rcpp::Named("call") = 1); 
+  tri.attr("class") = "tri";
   
   Rcpp::List res = Rcpp::List::create(Rcpp::Named("mesh") = mesh,
                                       Rcpp::Named("x") = coord,
-                                      Rcpp::Named("temp") 
-                                        = Rcpp::NumericVector::create(localbox.left, localbox.right, localbox.bottom, localbox.top));
-    
+                                      Rcpp::Named("tri.obj") = tri,
+                                      Rcpp::Named("temp") = Rcpp::NumericVector::create(localbox.left, localbox.right, localbox.bottom, localbox.top));
+  res.attr("class") = "delvor";
+  
   return res;
 }
 
 /*** R
-# sampling some random points
-n = 50
-errtest = 0; # dummy for testing exiting error (maybe this would be handled by R)
-set.seed(52)
-x = runif(n)
-y = runif(n+errtest)
-
 # loading required packages
 require(alphahull)
 require(rbenchmark)
 
-print("executing test")
+# sampling some random points
+n = 50
+errtest = 0; # dummy for testing exiting error (maybe this would be handled by R)
+
+set.seed(309)
+x = runif(n)
+y = runif(n+errtest)
+  
 vorcpp = computeVoronoiRcpp(x, y)
-vorR = delvor(x, y)
+vorR = delvor(x,  y)
+
 # benchmark(computeVoronoiRcpp(x, y), delvor(x, y))
 */
