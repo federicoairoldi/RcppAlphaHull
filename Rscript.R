dd138f3504164d1ae87369dfd1deb516902096ff
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-07-11 16:11:13 +0200

    updated the script

--- Rscript.R
+++ Rscript.R
@@ -5,7 +5,7 @@ sourceCpp("include/try.cpp")
 View(vorR$mesh)
 View(vorcpp$mesh)
 
-plot(vorR, xlim = vorcpp$temp[1:2], ylim = vorcpp$temp[3:4])
+plot(vorR, xlim = c(0,1), ylim = c(0,1))
 for(i in 1:dim(vorcpp$mesh)[1])
   lines(c(vorcpp$mesh[i, "mx1"], vorcpp$mesh[i, "mx2"]), 
         c(vorcpp$mesh[i, "my1"], vorcpp$mesh[i, "my2"]), 
@@ -14,8 +14,20 @@ for(i in 1:dim(vorcpp$mesh)[1])
 length(which(vorcpp$mesh[, "bp2"] == 1 | vorcpp$mesh[, "bp1"] == 1))
 length(which(vorR$mesh[, "bp2"] == 1 | vorR$mesh[, "bp1"] == 1))
 
-tmp = vorR$mesh[which(vorR$mesh[,"bp2"] == 1),]
+search = function(ind1, ind2, mesh){
+  for(i in 1:dim(mesh)[1]){
+      if(mesh[i,"ind1"] == ind1 & mesh[i,"ind2"] == ind2)
+        return(TRUE)
+      if(mesh[i,"ind1"] == ind2 & mesh[i,"ind2"] == ind1)
+        return(TRUE)
+    }
+  return(FALSE)
+}
+
+k = 0
+tmp = vorR$mesh[which(vorR$mesh[,"bp2"] == 1 | vorR$mesh[, "bp1"] == 1), 1:2]
+tmp2 = vorcpp$mesh[which(vorcpp$mesh[,"bp2"] == 1 | vorcpp$mesh[, "bp1"] == 1), 1:2]
 for(i in 1:dim(tmp)[1])
-  lines(c(tmp[i, "mx1"], tmp[i, "mx2"]), 
-        c(tmp[i, "my1"], tmp[i, "my2"]), col = "green")
+  k = k + !(search(tmp[i,"ind1"], tmp[i,"ind2"], tmp2))
+
 
