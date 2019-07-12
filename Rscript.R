623b87cad3d6300fc55ac0299354d0aa445dfa8b
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-07-12 18:04:00 +0200

    introduced my... functions testing

--- Rscript.R
+++ Rscript.R
@@ -5,7 +5,13 @@ sourceCpp("include/try.cpp")
 View(vorR$mesh)
 View(vorcpp$mesh)
 
-plot(vorR, xlim = c(0,1), ylim = c(0,1))
+x11()
+plot(vorR, col = c("blue", "red", "black", "blue"), pch = 19,
+     xlim = c(0,1), ylim = c(0,1), wlines = 'both', number = T)
+x11()
+plot.delvor.cpp(vorcpp, col = c("blue", "red", "black", "blue"), pch = 19,
+     xlim = c(0,1), ylim = c(0,1), wlines = 'both', number = T)
+
 for(i in 1:dim(vorcpp$mesh)[1])
   lines(c(vorcpp$mesh[i, "mx1"], vorcpp$mesh[i, "mx2"]), 
         c(vorcpp$mesh[i, "my1"], vorcpp$mesh[i, "my2"]), 
@@ -17,26 +23,24 @@ length(which(vorR$mesh[, "bp2"] == 1 | vorR$mesh[, "bp1"] == 1))
 system.time(computeVoronoiRcpp(x, y))
 system.time(delvor(x, y))
 
-set.seed(7658)
-n.nodes = c( seq(10,90,by=10), seq(100,900,by=100), seq(1000,10000,by=1000) )
-RTime = c()
-CppTime = c()
-for(n in n.nodes){
-  print(paste("Simulation:",n))
-  x = runif(n)
-  y = runif(n)
-  res = benchmark("Cpp" = computeVoronoiRcpp(x, y), "R" = delvor(x, y), replications = 1)
-  CppTime = rbind(CppTime, res[which(res[,"test"] == "Cpp"), c("elapsed", "user.self", "sys.self")])
-  RTime = rbind(RTime, res[which(res[,"test"] == "R"), c("elapsed", "user.self", "sys.self")])
-}
-tmp = cbind(CppTime, RTime)
-x11()
-matplot(n.nodes, tmp[,c(2,5)], type = "l", col = c("blue", "red"), lty = 1, 
-        xlab = "Number of nodes", ylab = "CPU time")
-lines(n.nodes, (0.000001)*n.nodes*log(n.nodes, 2), col = "green", lty = 2)
-lines(n.nodes, (0.000000001)*n.nodes^2, col = "pink", lty = 2)
-legend("topleft", legend = c("C++", "R", "nlogn"), fill = c("blue", "red", "green"), )
+n = 100
+set.seed(309)
+x = runif(n)
+y = runif(n)
 
+vorcpp = my.delvor(x,y)
+vorR = delvor(x, y)
 
+alpha = 0.05
+asR = ashape(vorR, alpha = alpha)
+plot(asR)
+ascpp = my.ashape(vorcpp, alpha = alpha)
+plot(ascpp)
 
+ahR = ahull(asR, alpha = alpha)
+plot(ahR)
+ahcpp = my.ahull(ascpp, alpha = alpha)
+plot(ahcpp)
 
+system.time(ashape(vorR, alpha = alpha))
+system.time(ashape(vorcpp, alpha = alpha))
