6729025edceb10e47f27f17a3ca2c24ed01adfe6
“federicoairoldi” <federico2.airoldi@mail.polimi.it>
2019-12-01 12:02:36 +0100

    corrected documentation of function inahull

--- RcppAlphahull/R/inahull.R
+++ RcppAlphahull/R/inahull.R
@@ -1,15 +1,14 @@
-#' Determines if the provided point fall inside the \eqn{\alpha}-hull or not.
+#' Determines if the provided points fall inside the \eqn{\alpha}-hull or not.
 #'
-#' Determines if the provided point fall inside the \eqn{\alpha}-hull or not calling an external C++
+#' Determines if the provided points fall inside the \eqn{\alpha}-hull or not by calling an external C++
 #' function.
 #'
 #' @param ahull.onj an object class "ahull" like the one returned by the function \code{\link{ahull}}
-#' @param x x coordinates of the sites. Alternatively, a matrix with two columns containing both sites 
+#' @param x x coordinates of the points to be checked. Alternatively, a matrix with two columns containing both 
 #' coordinates.
-#' @param y y coordinates of the sites.
+#' @param y y coordinates of the points to be checked.
 #'
-#' @return A list with the following components:
-#' A logical vector of the same length of the number of points and such that the i-th element denotes 
+#' @return A logical vector of the same length of the number of points and such that the i-th element denotes 
 #' if the i-th point is in the \eqn{\alpha}-hull or not.
 #'
 #' @seealso \code{\link{ahull}}, \code{\link{complement}}
